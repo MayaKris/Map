8e13c7425dda5b88a073326fe2adb54f68be273e
Maya Krishnan <mayark527@gmail.com>
2026-02-09 07:57:53 -0600

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Maya Krishnan on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
